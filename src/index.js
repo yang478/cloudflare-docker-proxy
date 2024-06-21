f53472b603d605ed24e2d7d1089556f56fd06c57
yang478 <55377570+yang478@users.noreply.github.com>
2024-06-21 23:41:00 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.youngzhong666.top": dockerHub,
+  "quay.youngzhong666.top": "https://quay.io",
+  "gcr.youngzhong666.top": "https://gcr.io",
+  "k8s-gcr.youngzhong666.top": "https://k8s.gcr.io",
+  "k8s.youngzhong666.top": "https://registry.k8s.io",
+  "ghcr.youngzhong666.top": "https://ghcr.io",
+  "cloudsmith.youngzhong666.top": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.youngzhong666.top": dockerHub,
 };
 
 function routeByHosts(host) {
